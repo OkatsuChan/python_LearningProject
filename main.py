db934ffc37b82f9e97412d01c5ba3d7408a154c4
Rage <151748793+OkatsuChan@users.noreply.github.com>
2024-09-07 21:15:48 +0800

    Variables

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 # My first python display program
+
 print("i love KareKare")
 print("Its really good")
 
 # My name
 first_name  = "Christian"
+food = "KareKare"
+email = "test@gmail.com"
+
+# int
+age = 25
+
+#float
+
+price = 10.99
+
 
 print(f"Hello {first_name}")
+
+print(f"You age is {age}")
+
+print(f"Price is  {price}")
+
+#Boolean
+
+is_student = False
+
+if is_student:
+    print("Yes youre a student")
+else:
+    print("Your not a student")
+
