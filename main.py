4c7b2a5f51710d9b306c4043e2a25fc28cde17d6
Rage <151748793+OkatsuChan@users.noreply.github.com>
2024-09-08 13:15:29 +0800

    Practice String Indexes

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-#
-#
-# name = input("Enter your full name: ")
-# phone = input("Enter your phone #: ")
-#
-# # result =len(name)
-# # result  = name.find("o")
-# #result  = name.rfind("s") # last occur of the letter we want to find
-# # result  = name.capitalize()
-# # result  = name.upper()
-# # result  = name.lower()
-# result  = name.isdigit()
-# # result  = name.isalpha()
-# # result  = phone.count("-")
-# result  = phone.replace("-"," ")
-#
-#
-#
-# print(result)
+
+credit_number = "1234-5678-9012-3456"
 
 
-username = input("Enter your username: ")
+# print(credit_number[4])
+# print(credit_number[:4])
+# print(credit_number[5:])
+# print(credit_number[-2])
+# print(credit_number[::3])
 
-length = len(username) +1
-if length < 12:
-    if username.find(" ") == -1:
-        if username.isalpha():
-            print("Successful Login")
-        else:
-            print("Dont add number on username")
-    else:
-        print("Dont put some space on username")
-else:
-    print("More than 12 character")
+credit_number = credit_number[::-1]
+print(f"{credit_number}")
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
