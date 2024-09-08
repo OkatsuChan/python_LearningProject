92e6fce346aa822ed7907147342aef00ab191d46
Rage <151748793+OkatsuChan@users.noreply.github.com>
2024-09-08 12:34:32 +0800

    Logical Operators Practice

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-#
-# age = int(input("Enter you age:"))
-#
-# if age > 100 :
-#     print("You're age is over 100")
-# elif age < 0:
-#     print("You're age is not allowed")
-# elif age >= 18:
-#     print("You're are now qualified")
-# else:
-#     print("You're are not qualified you must be 18 up")
 
-# response = input("Would you like food? (Y/N)")
-#
-# if response == "Y":
-#     print("Have some food!")
-# else:
-#     print("No food for you")
 
-# name = input("Enter your name: ")
-#
-# if name == "":
-#     print("You didnt type anything")
-# else:
-#     print(f"Hello {name}")
+temp = 25
 
-# for_sale = False
-#
-# if for_sale:
-#     print("This item is for sale")
-# else:
-#     print("This item is not for sale")
+sunny = True
 
-online = True
-
-if online:
-    print("This user is online")
+if temp  <= 0 or temp >= 30:
+    print("The temperature is bad")
 else:
-    print("This user is offline")
+    print("The temperature is good")
+
+if not sunny:
+    print("Its is cloudy outside")
+else:
+    print("It is sunny outside")
