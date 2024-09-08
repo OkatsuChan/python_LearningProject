1e42f064beba5fa86a2c272fa3e05b41a32fcacc
Rage <151748793+OkatsuChan@users.noreply.github.com>
2024-09-08 20:32:05 +0800

    Update game rockpaper scissor to much good approach

--- FirstDay/RockPaperScissor.py
+++ FirstDay/RockPaperScissor.py
@@ -26,36 +26,55 @@ scissors= '''
           (____)
     ---.__(___)
     '''
-choice = ["rock", "paper", "scissors"]
+choice = [rock, paper, scissors]
+user_choice = int(input("Enter your choice (0 for rock, 1 for paper, 2 for scissors): "))
 
-user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+bot_choice = random.randint(0,2)
 
-while user_choice not in choice:
-    user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
-
-bot_choice = random.choice(choice)
-
-if user_choice == "rock":
-    print(rock)
-elif user_choice == "paper":
-    print(paper)
-elif user_choice == "scissors":
-    print(scissors)
-
+print(choice[user_choice])
 print("Computer Chose")
-if bot_choice == "rock":
-    print(rock)
-elif bot_choice == "paper":
-    print(paper)
-elif bot_choice == "scissors":
-    print(scissors)
-
-
+print(choice[bot_choice])
 
 if user_choice == bot_choice:
     print("It's a tie!")
-elif user_choice == "rock" and bot_choice == "scissors" or user_choice == "scissors" and bot_choice == "paper" or user_choice == "paper" and bot_choice == "rock":
+elif user_choice == 0 and bot_choice == 2 or user_choice == 1 and bot_choice == 0 or user_choice == 2 and bot_choice == 1:
     print("You win!")
 else:
     print("You lose!")
+
+
+
+
+# choice = ["rock", "paper", "scissors"]
+#
+# user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+#
+# while user_choice not in choice:
+#     user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+#
+# bot_choice = random.choice(choice)
+#
+# if user_choice == "rock":
+#     print(rock)
+# elif user_choice == "paper":
+#     print(paper)
+# elif user_choice == "scissors":
+#     print(scissors)
+#
+# print("Computer Chose")
+# if bot_choice == "rock":
+#     print(rock)
+# elif bot_choice == "paper":
+#     print(paper)
+# elif bot_choice == "scissors":
+#     print(scissors)
+#
+#
+#
+# if user_choice == bot_choice:
+#     print("It's a tie!")
+# elif user_choice == "rock" and bot_choice == "scissors" or user_choice == "scissors" and bot_choice == "paper" or user_choice == "paper" and bot_choice == "rock":
+#     print("You win!")
+# else:
+#     print("You lose!")
 
