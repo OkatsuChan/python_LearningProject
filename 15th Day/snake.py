4ab340b22456a472ae1d555321dd6bed54290b1a
Rage <151748793+OkatsuChan@users.noreply.github.com>
2024-10-01 11:55:30 +0800

    Implemented: -Updating snake application having to have the user to control where the snake would go

--- 15th Day/snake.py
+++ 15th Day/snake.py
@@ -3,11 +3,20 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
 MOVE_DISTANCE = 20
+Up = 90
+Down = 270
+Left = 180
+Right = 0
+
+
+
+
 class Snake():
 
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for positions in STARTING_POSITIONS:
@@ -22,7 +31,22 @@ class Snake():
             new_x = self.segments[seg_num -1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x,new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
+    def up(self):
+        if self.head.heading() != Down :
+            self.head.setheading(Up)
+
+    def down(self):
+        if self.head.heading() != Up :
+            self.head.setheading(Down)
+
+    def left(self):
+        if self.head.heading() != Right:
+         self.head.setheading(Left)
+
+    def right(self):
+        if self.head.heading() != Left:
+            self.head.setheading(Right)
 
 
